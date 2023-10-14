636b55d2678d9b9aa170bbe24fc62b8eb6fde2b0
snilloO <pami.cvx@outlook.com>
2023-10-14 14:43:49 -0700

    initialize structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HLTV_stats_analysis
 Scripts to generate analysis about HLTV stats to give data support to CS pro player analysis
+# 
---
+++ item_class.py
@@ -0,0 +1,10 @@
+class Player(object):
+    pass
+class Team(object):
+    pass
+class Event(object):
+    pass
+class Coach(object):
+    pass
+class Map(object):
+    pass
