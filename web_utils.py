c2ddc05a73f9053db3c4ea52d099c07452da12e9
snilloO <pami.cvx@outlook.com>
2023-12-19 18:39:37 -0800

    update

--- web_utils.py
+++ web_utils.py
@@ -1,1 +1,8 @@
-import url
+import requests
+import bs4
+hltv_stats = "https://www.hltv.org/stats"
+hltv_filters = {'side':['COUNTER_TERRORIST','TERRORIST'],'startDate':['2023-08-18'],'endDate':['2023-11-1']}
+def batch_get_players(filters=None):
+    web = hltv_stats+'/players'
+    
+
